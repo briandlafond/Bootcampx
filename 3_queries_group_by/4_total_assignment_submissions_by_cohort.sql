dd46428eaee1dc9f1158b1dc066c5430b1abc7ff
Brian LaFond <brian.d.lafond@gmail.com>
2021-09-29 18:05:48 +0000

    add query for low average completion time by student

--- 3_queries_group_by/4_total_assignment_submissions_by_cohort.sql
+++ 3_queries_group_by/4_total_assignment_submissions_by_cohort.sql
@@ -3,4 +3,4 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY total_submissions DESC;
+ORDER BY total_submissions DESC; 
