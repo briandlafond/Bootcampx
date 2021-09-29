105278561e90891d3adbd5000317b6d40943deb0
Brian LaFond <brian.d.lafond@gmail.com>
2021-09-29 20:53:54 +0000

    refactor select clause)

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -1,4 +1,4 @@
-SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, assistance_requests.completed_at - assistance_requests.started_at as duration
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at - started_at) as duration
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
