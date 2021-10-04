a15d15cf74a2b56b3c51a6efcb25379d7b11ee1e
Brian LaFond <brian.d.lafond@gmail.com>
2021-10-04 20:13:37 +0000

    refactor to Parameterized Queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`]
+
 pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+SELECT DISTINCT cohorts.name as cohort, teachers.name as teacher
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${cohort.name}: ${teacher.name}`);
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`, values)
+.then(res => { 
+  console.log('connected')
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
