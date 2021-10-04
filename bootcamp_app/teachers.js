b7517ec1ac439434659b39846199b9d3a4be68c0
Brian LaFond <brian.d.lafond@gmail.com>
2021-10-04 20:16:01 +0000

    refactor

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -20,9 +20,8 @@ WHERE cohorts.name LIKE $1
 ORDER BY teacher
 `, values)
 .then(res => { 
-  console.log('connected')
   res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+    console.log(`${cohort.name}: ${teacher.name}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
